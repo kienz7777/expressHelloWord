ddb4ab8b5d2aa324ac3ca6c440949a786b15eb32
kienz7777 <kienvu729>
2020-04-21 22:44:25 +0700

    lesson2

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/',  (req,res) => {res.send('<h1>Hello Word!</h1>')});
-app.listen(port, () => {console.log('Server listening on port ' + port);})
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res) => { res.render('index', { name: 'kienz' }) });
+app.get('/users', (req, res) => {
+    res.render('user/index',
+        {
+            users: [
+                {id : 1, name : 'kien'},
+                {id : 2, name : 'ko'},
+                {id : 3, name : 'den'}
+               
+            ]
+        })
+});
+app.listen(port, () => { console.log('Server listening on port ' + port); })
