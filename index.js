3ff21262e22a391e567bcb396e0682260287311c
kienz7777 <kienvu729>
2020-04-27 21:56:13 +0700

    layouts

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.set('views', './views');
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+app.use(express.static('public'));
 
 app.get('/', (req, res) => { res.render('index', { name: 'kienz' }) });
 
