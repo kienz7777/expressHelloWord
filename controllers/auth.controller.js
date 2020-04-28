e3acdf3745cdfe466c3ed52ddb4e0134ad7750c8
kienz7777 <kienvu729>
2020-04-28 21:12:51 +0700

    signedCookies

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -36,6 +36,8 @@ module.exports.postLogin =  (req, res) => {
         return;
     }
 
-    res.cookie('userId', user.id);
+    res.cookie('userId', user.id,{
+        signed : true
+    });
     res.redirect('/users');
 };
